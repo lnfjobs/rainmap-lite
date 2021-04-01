168edeb98f91384b71288739774e2d12e9ed1460
lnfjobs <67797515+lnfjobs@users.noreply.github.com>
2021-03-31 22:16:29 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sed -i "s/8000/8080/g" "nmaper-cronjob.py"
+echo What is your public IP address?
+read ip
+sed -i "s/127.0.0.1/$ip/g" "nmaper-cronjob.py"
+echo What is your SMTP user name?
+read user
+sed -i "s/youremail@gmail.com/$user/g" "nmaper-cronjob.py"
+echo What is your SMTP password?
+read pass
+sed -i "s/yourpassword/$pass/g" "nmaper-cronjob.py"
+echo What is your SMTP address?
+read smtp
+sed -i "s/smtp.gmail.com/$smtp/g" "nmaper-cronjob.py"
+python manage.py migrate
+python manage.py loaddata nmapprofiles
+python manage.py createsuperuser
+python manage.py runserver 0.0.0.0:8080 &
+while true
+do
+python nmaper-cronjob.py
+sleep 15
+done
