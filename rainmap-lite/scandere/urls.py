e2a84dd7cdbd13960f347cb756aa51ee800a9afe
lnfjobs <67797515+lnfjobs@users.noreply.github.com>
2021-04-05 21:26:10 -0400

    Update urls.py

--- rainmap-lite/scandere/urls.py
+++ rainmap-lite/scandere/urls.py
@@ -18,6 +18,8 @@ from nmaper.admin import views as adm_views
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as views_django
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^console/clear_logs/?$', adm_views.clear_logs, name='clear-logs'),
